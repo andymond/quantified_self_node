0ad15df23d177c367da6be914672454e8acde090
Andrew Dymond <andy.dymond@gmail.com>
2018-05-03 15:42:26 -0600

    Add '/foods returns list of all foods' test to test-food-endpoints

--- test/test-food-endpoints.js
+++ test/test-food-endpoints.js
@@ -6,11 +6,22 @@ const chaiHTTP = require('chai-http')
 chai.use(chaiHTTP)
 const requester = chai.request(app)
 
-describe('test', function() {
-  it('test setup', function() {
-    requester.get('/').end(function (err, res) {
+describe('api/v1 foods endpoints', function() {
+  it('/foods returns list of all foods', function() {
+    requester.get('/api/v1/foods').end(function (err, res) {
       expect(err).to.be.null
-      expect(res).to.have.status(200);
+      expect(res).to.have.status(200)
+
+      let results = JSON.parse(res.body)
+
+      expect(results).to.be.an('array')
+      expect(results).count.to.eq(3)
+      expect(results[0].id).to.eq(1)
+      expect(results[0].id).to.eq("testfood1")
+      expect(results[0].id).to.eq(100)
+      expect(results[2].id).to.eq(3)
+      expect(results[2].id).to.eq("testfood3")
+      expect(results[2].id).to.eq(1000)
     });
   });
 });
