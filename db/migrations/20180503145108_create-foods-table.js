e657eb6d0343d9f6a645099cb231c14fca1c4f0c
Andrew Dymond <andy.dymond@gmail.com>
2018-05-03 15:26:25 -0600

    Add seed file

--- db/migrations/20180503145108_create-foods-table.js
+++ db/migrations/20180503145108_create-foods-table.js
@@ -2,9 +2,8 @@
 exports.up = function(knex, Promise) {
   let createQuery = `CREATE TABLE foods(
     id SERIAL PRIMARY KEY NOT NULL,
-    name STRING,
-    calories INTEGER,
-    created_at TIMESTAMP
+    name TEXT,
+    calories INTEGER
   )`
   return knex.raw(createQuery)
 };
