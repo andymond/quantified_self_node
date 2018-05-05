a5e35672cc7856540f727b9b5489b3a6562bfc13
Andrew Dymond <andy.dymond@gmail.com>
2018-05-05 15:50:30 -0600

    Add food class with all query method

--- models/food.js
+++ models/food.js
@@ -1,3 +1,9 @@
 const environment = process.env.NODE_ENV || 'development'
 const configuration = require('../knexfile')[environment]
 const database = require('knex')(configuration)
+
+module.exports = class Food {
+  static all() {
+    return database('foods').select('id', 'name', 'calories')
+  }
+}
