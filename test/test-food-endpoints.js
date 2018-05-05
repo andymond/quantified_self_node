762452fd932d066e0766933cc722812b5663b397
Andrew Dymond <andy.dymond@gmail.com>
2018-05-05 13:59:27 -0600

    Add test seed file & adjust 'GET /foods returns list of all foods'

--- test/test-food-endpoints.js
+++ test/test-food-endpoints.js
@@ -1,38 +1,58 @@
+const environment = process.env.NODE_ENV || 'test';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
 const app = require('../app.js')
 const chai = require('chai')
-const expect = chai.expect
-const assert = require('assert')
+const should = chai.should();
 const chaiHTTP = require('chai-http')
 chai.use(chaiHTTP)
 
 describe('api/v1 foods endpoints', function() {
+  before((done) => {
+    database.migrate.latest()
+    .then(() => done())
+    .catch((error) => {
+      throw error;
+    })
+    .done();
+  })
+
+  beforeEach((done) => {
+    database.seed.run()
+    .then(() => done())
+    .catch((error) => {
+      throw error;
+    })
+    .done();
+  })
   this.timeout(0)
   it('should return a 404 with text', () => {
-    chai.request(app)
+    return chai.request(app)
       .get('/sad')
       .then((response) => {
-        expect(response.status).to.eq(404);
-        expect(response.text).to.include("Not Found")
+        response.should.have.status(404);
+        response.text.should.include("Not Found")
       })
       .catch((error) => {
         throw error;
       });
   });
 
-  it('/foods returns list of all foods', function() {
-    chai.request(app)
+  it('GET /foods returns list of all foods', () => {
+    return chai.request(app)
       .get('/api/v1/foods')
       .then((response) => {
-        expect(response.status).to.eq(200)
-        expect(response).to.be.json
-        expect(response).to.be.an('array')
-        expect(response).count.to.eq(3)
-        expect(response[0].id).to.eq(1)
-        expect(response[0].id).to.eq("testfood1")
-        expect(response[0].id).to.eq(100)
-        expect(response[2].id).to.eq(3)
-        expect(response[2].id).to.eq("testfood3")
-        expect(response[2].id).to.eq(1000)
+        response.should.have.status(200)
+        response.should.be.json
+        response.body.should.be.a('array')
+        response.body.count.should.eq(3)
+        response.body[0].id.should.eq(1)
+        response.body[0].name.should.eq("banana")
+        response.body[0].calories.should.eq(100)
+        response.body[2].id.should.eq(3)
+        response.body[2].name.should.eq("ham sandwich")
+        response.body[2].id.should.eq(700)
       })
       .catch((error) => {
         throw error
