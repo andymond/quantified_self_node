f4e9789b257a6e6a6fc5d24998f52f7d86a19103
Andrew Dymond <andy.dymond@gmail.com>
2018-05-07 10:13:06 -0600

    Add all() to meal model

---
+++ models/meal.js
@@ -0,0 +1,17 @@
+const environment = process.env.NODE_ENV || 'development'
+const configuration = require('../knexfile')[environment]
+const database = require('knex')(configuration)
+
+module.exports = class Meal {
+  static all() {
+    return database('meals')
+      .leftJoin('mealfoods', 'meals.id', 'mealfoods.meal_id')
+      .leftJoin('foods', 'mealfoods.food_id', 'foods.id')
+      .select(['meals.id',
+               'meals.name',
+               database.raw('JSON_AGG(foods.*) as foods')
+             ])
+      .groupBy('meals.id')
+      .orderBy('meals.id')
+  }
+}
