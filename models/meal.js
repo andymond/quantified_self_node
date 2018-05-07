3a4540e9ea60dd135baeced6fc703983b393ef8d
Andrew Dymond <andy.dymond@gmail.com>
2018-05-07 10:40:16 -0600

    Add find(id) to meal model

--- models/meal.js
+++ models/meal.js
@@ -14,4 +14,15 @@ module.exports = class Meal {
       .groupBy('meals.id')
       .orderBy('meals.id')
   }
+
+  static find(id) {
+    return database('meals').where('meals.id', id)
+      .leftJoin('mealfoods', 'meals.id', 'mealfoods.meal_id')
+      .leftJoin('foods', 'mealfoods.food_id', 'foods.id')
+      .select(['meals.id',
+               'meals.name',
+               database.raw('JSON_AGG(foods.*) as foods')
+             ])
+      .groupBy('meals.id')
+  }
 }
