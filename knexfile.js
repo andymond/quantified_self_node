74d2a9803fb85b812567ac2b4d3bd34467d1ee08
Andrew Dymond <andy.dymond@gmail.com>
2018-05-08 15:26:53 -0600

    Add migrations config for production

--- knexfile.js
+++ knexfile.js
@@ -30,12 +30,14 @@ module.exports = {
     client: 'postgresql',
     connection: process.env.DATABASE_URL,
     pool: {
-
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/production'
     }
   }
 
