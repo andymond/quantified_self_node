4ae72f6e00d2cf67eea8f85899acb604d42d53a6
Andrew Dymond <andy.dymond@gmail.com>
2018-05-08 14:54:30 -0600

    Add cors package & basic app config

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var createError = require('http-errors');
+var cors = require('cors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -11,6 +12,7 @@ var foodsRouter = require('./routes/api/v1/foods')
 var mealsRouter = require('./routes/api/v1/meals')
 
 var app = express();
+app.use(cors())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
